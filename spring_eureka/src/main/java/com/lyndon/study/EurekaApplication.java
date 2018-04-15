50561d15dbfa34427afb806f784d371456019e2c
lyndon_wucb <wucb@innapp.cn>
2018-04-16 00:28:22 +0800

    注册中心

--- spring_eureka/src/main/java/com/lyndon/study/EurekaApplication.java
+++ spring_eureka/src/main/java/com/lyndon/study/EurekaApplication.java
@@ -1,7 +1,7 @@
 package com.lyndon.study;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
@@ -12,7 +12,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class EurekaApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EurekaApplication.class,args);
+        new SpringApplicationBuilder(EurekaApplication.class)
+                .web(true).run(args);
     }
 
 }
