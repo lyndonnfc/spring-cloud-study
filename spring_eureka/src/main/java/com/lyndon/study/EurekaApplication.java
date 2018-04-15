7558a90835c713df1c57064336ecb10ae0e4d88c
lyndon_wucb <wucb@innapp.cn>
2018-04-15 21:45:19 +0800

    项目初始化

---
+++ spring_eureka/src/main/java/com/lyndon/study/EurekaApplication.java
@@ -0,0 +1,18 @@
+package com.lyndon.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by lyndon on 2018/4/14.
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class,args);
+    }
+
+}
